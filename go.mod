ed2d28254cd930ab0356216b9552bbc167b5925d
sre-priscilla <943459049@qq.com>
2021-02-06 15:10:05 +0800

    log

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module hiahia
 go 1.15
 
 require (
-	github.com/labstack/echo-contrib v0.9.0 // indirect
-	github.com/labstack/echo/v4 v4.1.17 // indirect
+	github.com/labstack/echo-contrib v0.9.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/gommon v0.3.0
 )
--- main.go
+++ main.go
@@ -5,16 +5,27 @@ import (
 
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := echo.New()
+	e.HideBanner = true
+	e.Logger.SetLevel(log.INFO)
 
 	p := prometheus.NewPrometheus("hiahia", nil)
 	p.Use(e)
 
 	e.GET("/routes", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, e.Routes())
+	})
+	e.GET("/say", func(c echo.Context) error {
+		e.Logger.Error("someone say something")
+		return c.JSON(http.StatusOK, echo.Map{"foo": "bar"})
+	})
+	e.GET("/smile", func(c echo.Context) error {
+		e.Logger.Info("someone smiled")
+		return c.JSON(http.StatusOK, echo.Map{"hello": "world"})
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
